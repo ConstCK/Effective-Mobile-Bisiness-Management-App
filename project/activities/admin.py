66d341c4905887152fd5e0727adeff45899e29f0
Constantin <Constantin_CK@outlook.com>
2025-02-04 18:31:29 +0300

    feat: Register all models for admin panel

--- project/activities/admin.py
+++ project/activities/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import News, Meeting, Task, TaskStatus, TaskEstimation, Calendar
 
-# Register your models here.
+admin.site.register(News)
+admin.site.register(Meeting)
+admin.site.register(Task)
+admin.site.register(TaskStatus)
+admin.site.register(TaskEstimation)
+admin.site.register(Calendar)
--- project/companies/admin.py
+++ project/companies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Company, Structure, StructureMember
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Structure)
+admin.site.register(StructureMember)
